cda7ddfffd67e12d66426b3835205c75a0422c51
StevenLino <stemlino@espol.edu.ec>
2025-01-30 01:54:33 -0500

    Mostrar todos los datos por pantalla

--- main/views.py
+++ main/views.py
@@ -8,6 +8,9 @@ import json
 # Importe el decorador login_required
 from django.contrib.auth.decorators import login_required, permission_required
 
+from collections import Counter
+from datetime import datetime
+
 # Restricción de acceso con @login_required
 @login_required
 @permission_required('main.index_viewer', raise_exception=True)
@@ -29,11 +32,39 @@ def index(request):
     # Valores de la respuesta
     responses = response_dict.values()
 
+    # Obtener el primer elemento del JSON
+    first_key = next(iter(response_dict))
+    first_email = response_dict[first_key]["email"]
+
+    # Dividir el correo electrónico en dos partes usando '@'
+    first_response = first_email.split('@')[0]
+
+    #Obteniendo la última respuesta
+    last_key = next(reversed(response_dict))
+    last_email = response_dict[last_key]["email"]
+    last_response = last_email.split('@')[0]
+
+    # Contador para almacenar la cantidad de respuestas por día
+    date_counter = Counter()
+
+    for entry in response_dict.values():
+        saved_date = entry.get("saved", "")  # Obtener la fecha guardada
+        if saved_date:
+            date_part = saved_date.split(",")[0]  # Extraer solo la fecha
+            date_counter[date_part] += 1
+
+    # Encontrar el día con más respuestas
+    most_common_date, max_responses = date_counter.most_common(1)[0] if date_counter else ("No data", 0)
+
     # Objeto con los datos a renderizar
     data = {
         'title': 'Landing - Dashboard',
         'total_responses': total_responses,
-        'responses': responses
+        'responses': responses,
+        'first_response': first_response,
+        'last_response': last_response,
+        'most_common_date': most_common_date,
+        'max_responses': max_responses,
     }
 
     # Renderización en la plantilla
